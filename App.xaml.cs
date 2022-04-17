3ce8c65116880066a727d6e1bc080dfbc7b9b180
Percy Herrera Valdez <percy_j98@hotmail.com>
2022-04-16 23:31:45 -0500

    formulario con WPF

---
+++ App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows;
+
+namespace WPFSemana04_DesAEA
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Data;
+using System.Data.SqlClient;
+
+namespace WPFSemana04_DesAEA
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    
+    public partial class MainWindow : Window
+    {
+        SqlConnection connection = new SqlConnection(@"data source=DESKTOP-87FCA55\SQLEXPRESS;initial catalog = lab04martes_db; Integrated Security = True;");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Person> people = new List<Person>();
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("BuscarPersonaNombre", connection);
+            command.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter parameter1 = new SqlParameter();
+            parameter1.SqlDbType = SqlDbType.VarChar;
+            parameter1.Size = 50;
+
+            parameter1.Value = "";
+            parameter1.ParameterName = "@FirstName";
+
+            command.Parameters.Add(parameter1);
+
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                people.Add(new Person
+                {
+                    PersonId = dataReader[0].ToString(),
+                    LastName = dataReader[1].ToString(),
+                    FirstName = dataReader[2].ToString(),
+                    HireDate = dataReader[3].ToString(),
+                    EnrollmentDate = dataReader[4].ToString(),
+                });
+
+            }
+            connection.Close();
+            dgvPeople.ItemsSource = people;
+        }
+
+    }
+}
