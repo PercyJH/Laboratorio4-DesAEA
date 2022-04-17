f01540b1156e4aaca8c881a7323d42db48dee172
Percy Herrera Valdez <percy_j98@hotmail.com>
2022-04-16 23:29:20 -0500

    form1 && formulario persona

---
+++ Persona.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace Lab04___DesAEA
+{
+    partial class Persona
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnListar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.dgvListado = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(12, 12);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 0;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(114, 14);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(143, 20);
+            this.txtNombre.TabIndex = 1;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(263, 12);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 2;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // dgvListado
+            // 
+            this.dgvListado.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvListado.Location = new System.Drawing.Point(12, 42);
+            this.dgvListado.Name = "dgvListado";
+            this.dgvListado.Size = new System.Drawing.Size(326, 303);
+            this.dgvListado.TabIndex = 3;
+            // 
+            // Persona
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 357);
+            this.Controls.Add(this.dgvListado);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnListar);
+            this.Name = "Persona";
+            this.Text = "Persona";
+            this.Load += new System.EventHandler(this.Persona_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvListado)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.DataGridView dgvListado;
+    }
+}
+
---
+++ Persona.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab04___DesAEA
+{
+    public partial class Persona : Form
+    {
+        SqlConnection conn;
+        public Persona(SqlConnection conn)
+        {
+            this.conn = conn;
+            InitializeComponent();
+        }
+
+        private void Persona_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Open)
+                {
+                    String sql = "SELECT * FROM People";
+                    SqlCommand cmd = new SqlCommand(sql, conn);
+                    SqlDataReader reader1 = cmd.ExecuteReader();
+
+                    DataTable dt = new DataTable();
+                    dt.Load(reader1);
+                    dgvListado.DataSource = dt;
+                    dgvListado.Refresh();
+                }
+                else
+                {
+                    MessageBox.Show("La conexión está cerrada.");
+                }
+            }
+            catch (Exception exception1)
+            {
+                MessageBox.Show("No se pueden leer usuarios: \n" +
+                                 exception1.ToString());
+            }
+}
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //MÉTODO ANTERIOR
+
+            //if (conn.State == ConnectionState.Open)
+            //{
+            //    String FirstName = txtNombre.Text;
+
+            //    SqlCommand cmd = new SqlCommand();
+            //    cmd.CommandText = "BuscarPersonaNombre";
+            //    cmd.CommandType = CommandType.StoredProcedure;
+            //    cmd.Connection = conn;
+
+            //    SqlParameter param = new SqlParameter();
+            //    param.ParameterName = "@FirstName";
+            //    param.SqlDbType = SqlDbType.NVarChar;
+            //    param.Value = FirstName;
+
+            //    cmd.Parameters.Add(param);
+
+            //    SqlDataReader reader2 = cmd.ExecuteReader();
+
+            //    DataTable dt = new DataTable();
+            //    dt.Load(reader2);
+            //    dgvListado.DataSource = dt;
+            //    dgvListado.Refresh();
+
+            //}
+            //else
+            //{
+            //    MessageBox.Show("La conexión está cerrada.");
+            //}
+
+            //-----------------------------------------------------------------------------//
+
+            //MÉTODO SIN DATATABLE
+
+            List<Personas> personas = new List<Personas>();
+
+            SqlCommand command = new SqlCommand("BuscarPersonaNombre", conn);
+            command.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter param1 = new SqlParameter();
+            //SqlDbType Varchar 
+            param1.Value = txtNombre.Text.Trim();
+            param1.ParameterName = "@FirstName";
+            param1.SqlDbType = SqlDbType.VarChar;
+
+            command.Parameters.Add(param1);
+
+            SqlDataReader reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                personas.Add(new Personas
+                {
+                    PersonId = Convert.ToInt32(reader["PersonID"]),
+                    FirstName = Convert.ToString(reader["FirstName"]),
+                    LastName = Convert.ToString(reader["LastName"]),
+                    HireDate = Convert.ToString(reader["HireDate"]),
+                    EnrollmentDate = Convert.ToString(reader["EnrollmentDate"]),
+                }
+                );
+            }
+
+            dgvListado.DataSource = personas;
+        }
+    }
+}
